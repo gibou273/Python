3b8f9b3aea728ac201cec8c5acd7b6027b3440aa
gibou273 <101921033+gibou273@users.noreply.github.com>
2024-02-08 17:37:41 +0100

    Collison with wall and reset game
    Added detection of collision with the wall, and if the snake collides with the wall then the snake will be sent to a new position outside the game window where it will be destroyed and a new snake will be created at the  center of the screen and start moving.

--- Python_Turtle_Graphics/snake_game/main.py
+++ Python_Turtle_Graphics/snake_game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -21,12 +23,24 @@ screen.onkey(key="Right", fun=snake.move_right)
 
 is_snake_moving = True
 while is_snake_moving:
-    time.sleep(0.1)
     screen.update()
+    time.sleep(0.1)
     snake.move_snake()
     # Detect collision with food
     if snake.snake_head.distance(food) < 15:
-        food.move_food()
+        food.move_food() # The food will be moved to a new random position
+        snake.extend_snake() # A new segment will be created and added to the snake, so the snake will be longer
+        scoreboard.increase_score() # The playes score will be increased
+    # Detect collison with wall
+    if snake.snake_head.xcor() < -280 or snake.snake_head.xcor() > 280 or snake.snake_head.ycor() < -280 or snake.snake_head.ycor() > 280:
+        scoreboard.reset_scoreboard()
+        snake.reset_snake()
+    # Detect collision with snake tail
+    for segment in snake.snake_segments[1:]:
+        if snake.snake_head.distance(segment) < 10:
+            scoreboard.reset_scoreboard()
+            snake.reset_snake()
+
 
 
 screen.exitonclick()
--- Python_Turtle_Graphics/snake_game/snake.py
+++ Python_Turtle_Graphics/snake_game/snake.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 
 POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVER_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -13,27 +14,46 @@ class Snake():
         self.snake_head = self.snake_segments[0]
     def create_snake(self):
         for position in POSITIONS:
-            new_snake = Turtle()
-            new_snake.shape("square")
-            new_snake.color("white")
-            new_snake.penup()
-            new_snake.goto(position)
-            self.snake_segments.append(new_snake)
+            self.add_segment(position)
+    def add_segment(self, position):
+            new_snake_segment = Turtle()
+            new_snake_segment.shape("square")
+            new_snake_segment.color("white")
+            new_snake_segment.penup()
+            new_snake_segment.goto(position)
+            self.snake_segments.append(new_snake_segment)
     def move_snake(self):
         for segment_num in range(len(self.snake_segments) - 1, 0, -1):
             new_posX = self.snake_segments[segment_num - 1].xcor()
             new_posY = self.snake_segments[segment_num - 1].ycor()
             self.snake_segments[segment_num].goto(x=new_posX, y=new_posY)
-        self.snake_head.forward(20)
+        self.snake_head.forward(MOVER_DISTANCE)
     def move_up(self):
-        if self.snake_head.heading != DOWN:
+        if self.snake_head.heading() != DOWN:
             self.snake_head.setheading(UP)
     def move_down(self):
-        if self.snake_head.heading != UP:
+        if self.snake_head.heading() != UP:
             self.snake_head.setheading(DOWN)
     def move_left(self):
-        if self.snake_head.heading != RIGHT:
+        if self.snake_head.heading() != RIGHT:
             self.snake_head.setheading(LEFT)
     def move_right(self):
-        if self.snake_head.heading != LEFT:
+        if self.snake_head.heading() != LEFT:
             self.snake_head.setheading(RIGHT)
+    """def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.snake_segments.append(new_segment)"""
+
+    def reset_snake(self):
+        # Loop through the list of snake segments and send all of them to a location outside the screen
+        for segment in self.snake_segments:
+            segment.goto(1000, 1000)
+        self.snake_segments.clear()
+        self.create_snake()
+        self.snake_head = self.snake_segments[0]
+
+    def extend_snake(self):
+        self.add_segment(self.snake_segments[-1].position())
