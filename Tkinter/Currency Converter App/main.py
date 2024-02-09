d55781f756ad45e4442b610291ad8c40bb75d18e
gibou273 <101921033+gibou273@users.noreply.github.com>
2024-02-09 01:29:08 +0100

    First Commit
    Created the currency converter window/screen

---
+++ Tkinter/Currency Converter App/main.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Label, Button, Canvas
+
+window = Tk()
+window.config(width=400, height=500)
+window.title("Currency Converter")
+
+
+
+window.mainloop()
---
+++ Tkinter/Scientific Calculater App/main.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Label, Button, Canvas
+
+window = Tk()
+window.config(width=400, height=500)
+window.title("Scientific Calculator")
+
+
+
+window.mainloop()
